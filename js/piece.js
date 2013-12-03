f5d033f892936ce8b43ef70d2c7195a727a1f98f
Timur Kristóf <venemo@msn.com>
2013-12-03 17:47:05 +0100

    minor refactor on Piece

--- js/piece.js
+++ js/piece.js
@@ -13,79 +13,6 @@ PM.Piece = (function () {
         this.y = this.supposedPosition.y;
         this.transformOrigin = { x: image.width / 2, y: image.height / 2 };
         this.grabbedTouches = [];
-        
-        var that = this;
-        
-        that.containsPoint = function (sx, sy) {
-            // Transform coordinates to the coordinate system of the piece
-            var p = that.mapFromScene({ x: sx, y: sy });
-            
-            // Rule out cases when the user clicked outsite the image
-            if (p.x < 0 || p.x > that.image.width || p.y < 0 || p.y > that.image.height) {
-                return false;
-            }
-            
-            // Check canvas image data
-            var ctx = that.image.getContext('2d');
-            var imageData = ctx.getImageData(p.x, p.y, 1, 1);
-            return imageData.data[3] > 0;
-            
-            // TODO: skip checking image data for obvious places (like, the rectangle which surely doesn't belong to neither tabs nor blanks
-        };
-        
-        that.draw = function (ctx) {
-            ctx.save();
-            ctx.translate(that.x, that.y);
-            ctx.rotate(that.angle);
-            ctx.drawImage(that.image, 0, 0);
-            ctx.restore();
-        };
-        
-        that.mapToScene = function(p0) {
-            var p = {
-                x: p0.x - that.transformOrigin.x,
-                y: p0.y - that.transformOrigin.y
-            };
-            var r = {
-                x: p.x * Math.cos(-that.angle) + p.y * Math.sin(-that.angle),
-                y: (-p.x) * Math.sin(-that.angle) + p.y * Math.cos(-that.angle)
-            };
-            
-            return {
-                x: that.x + r.x + that.transformOrigin.x,
-                y: that.y + r.y + that.transformOrigin.y
-            };
-        };
-        
-        that.mapFromScene = function(p0) {
-            var to_s = that.mapToScene(that.transformOrigin);
-            var p = {
-                x: p0.x - to_s.x,
-                y: p0.y - to_s.y
-            };
-            var r = {
-                x: p.x * Math.cos(that.angle) + p.y * Math.sin(that.angle),
-                y: (-p.x) * Math.sin(that.angle) + p.y * Math.cos(that.angle)
-            };
-            
-            return {
-                x: r.x + that.transformOrigin.x,
-                y: r.y + that.transformOrigin.y
-            };
-        };
-        
-        that.startDrag = function (sx, sy) {
-            that.dragStart = { x: sx - that.x, y: sy - that.y };
-        };
-        
-        that.doDrag = function (sx, sy) {
-            that.x = sx - that.dragStart.x;
-            that.y = sy - that.dragStart.y;
-        };
-        
-        that.checkMergeability = function (other) {
-            // TODO
-        };
     };
     
     Piece.prototype.x = 0;
@@ -94,6 +21,77 @@ PM.Piece = (function () {
     Piece.prototype.dragStart = null;
     Piece.prototype.transformOrigin = null;
     Piece.prototype.grabbedTouches = null;
+        
+    Piece.prototype.draw = function (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.drawImage(this.image, 0, 0);
+        ctx.restore();
+    };
+        
+    Piece.prototype.startDrag = function (sx, sy) {
+        this.dragStart = { x: sx - this.x, y: sy - this.y };
+    };
+    
+    Piece.prototype.doDrag = function (sx, sy) {
+        this.x = sx - this.dragStart.x;
+        this.y = sy - this.dragStart.y;
+    };
+    
+    Piece.prototype.checkMergeability = function (other) {
+        // TODO
+    };
+    
+    Piece.prototype.containsPoint = function (sx, sy) {
+        // Transform coordinates to the coordinate system of the piece
+        var p = this.mapFromScene({ x: sx, y: sy });
+        
+        // Rule out cases when the user clicked outsite the image
+        if (p.x < 0 || p.x > this.image.width || p.y < 0 || p.y > this.image.height) {
+            return false;
+        }
+        
+        // Check canvas image data
+        var ctx = this.image.getContext('2d');
+        var imageData = ctx.getImageData(p.x, p.y, 1, 1);
+        return imageData.data[3] > 0;
+        
+        // TODO: skip checking image data for obvious places (like, the rectangle which surely doesn't belong to neither tabs nor blanks
+    };
+        
+    Piece.prototype.mapToScene = function(p0) {
+        var p = {
+            x: p0.x - this.transformOrigin.x,
+            y: p0.y - this.transformOrigin.y
+        };
+        var r = {
+            x: p.x * Math.cos(-this.angle) + p.y * Math.sin(-this.angle),
+            y: (-p.x) * Math.sin(-this.angle) + p.y * Math.cos(-this.angle)
+        };
+        
+        return {
+            x: this.x + r.x + this.transformOrigin.x,
+            y: this.y + r.y + this.transformOrigin.y
+        };
+    };
+    
+    Piece.prototype.mapFromScene = function(p0) {
+        var to_s = this.mapToScene(this.transformOrigin);
+        var p = {
+            x: p0.x - to_s.x,
+            y: p0.y - to_s.y
+        };
+        var r = {
+            x: p.x * Math.cos(this.angle) + p.y * Math.sin(this.angle),
+            y: (-p.x) * Math.sin(this.angle) + p.y * Math.cos(this.angle)
+        };
+        
+        return {
+            x: r.x + this.transformOrigin.x,
+            y: r.y + this.transformOrigin.y
+        };
+    };
     
     return Piece;
     
